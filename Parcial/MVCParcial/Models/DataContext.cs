d9e0d9266810c24525b0eba22e5d815f69d4b93b
yamileandreaorellanasejas@gmail.com <yamileandreaorellanasejas@gmail.com>
2019-03-30 10:59:03 -0400

    Commit con Modelo MVC

---
+++ Parcial/MVCParcial/Models/DataContext.cs
@@ -0,0 +1,14 @@
+﻿using System.Data.Entity;
+                        // eliminos las librerias con control + .
+namespace MVCParcial.Models
+{
+    public class DataContext:DbContext                          //DbContex y damos control + . el primero
+    {
+        public DataContext(): base("DefaultConnection")             // damos ctor tab tab
+        {
+
+        }
+
+        public System.Data.Entity.DbSet<MVCParcial.Models.Product> Products { get; set; }
+    }
+}
---
+++ Parcial/MVCParcial/Models/Product.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MVCParcial.Models
+{
+    public enum TipoEstado {
+        Activo,
+        Inactivo
+    }
+    public enum TipoUnidad
+    {
+        Botella,
+        Metro,
+        Litro,
+        Caja
+    }
+    public class Product
+    {
+        [Key]
+        public int ProductId { get; set; }             //damos prop y tab tab seguido y nos da toda esa propiedad
+                                         
+        [Required]                                      //le damos control Punto en required y elimina
+        [Display(Name = "Product Name")]
+        public string Nombre { get; set; }
+     
+        public decimal Precio { get; set; }
+        [Required]
+        public int Cantidad { get; set; }
+
+        [Required]
+        public TipoUnidad Unidad { get; set; }
+
+        [Required]
+        public TipoEstado Estado { get; set; }
+
+
+    }
+}
